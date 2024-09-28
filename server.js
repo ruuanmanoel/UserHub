991fd5709a4f322d6fcc2143e9ea0d30ff25d2cd
Ruan Manoel <ruuanmanoel@gmail.com>
2024-09-28 15:29:13 -0300

    :sparkles: feat: importação do das rotas privadas e do middleware de validação

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
+import privateRoutes from './routes/private.js'
+import auth from './middlewares/auth.js'
+
 const PORT = 3035
 const app = express();
 app.use(express.json());
 app.use('/', publicRoutes)
+app.use('/', auth, privateRoutes)
 
 
 
