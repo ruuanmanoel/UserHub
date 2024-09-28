b77a913e872b60e7304b21d18fc212f772ed9eca
Ruan Manoel <ruuanmanoel@gmail.com>
2024-09-28 13:59:32 -0300

    :sparkles: feat: criptografia da senha e cadastro do user no BD

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const router = express.Router();
 
-router.post('/cadastro', (req, res)=>{
-    const user = req.body;
+router.post('/cadastro', async (req, res)=>{
+    try {
+        const user = req.body;
 
-    res.status(201).json(user)
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(user.password, salt)
+
+        const userDB = await prisma.user.create({
+            data:{
+                email:  user.email,
+                name: user.name,
+                password: hashPassword,
+            }
+        })
+        res.status(201).json(userDB)
+    } catch (error) {
+        res.status(500).json({message:"Erro no servidor, tente novamente"})
+    }
 });
 
 
